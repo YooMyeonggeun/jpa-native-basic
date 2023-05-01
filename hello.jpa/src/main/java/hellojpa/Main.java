fd1a13af33db98eedfab7c930a9457df4fc2c4e1
UMK <haga5032@naver.com>
2023-05-01 11:46:37 +0900

    엔티티 팩토리 구현

--- hello.jpa/src/main/java/hellojpa/Main.java
+++ hello.jpa/src/main/java/hellojpa/Main.java
@@ -1,7 +1,15 @@
 package hellojpa;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
 	public static void main(String[] args) {
+	
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		
+		emf.close();
+		System.out.println("hello");
 		
 	}
 }
