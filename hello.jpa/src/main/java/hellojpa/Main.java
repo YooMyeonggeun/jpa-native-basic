e0bf933da63c5e86603fbdba607d04313f23a34c
UMK <haga5032@naver.com>
2023-05-01 11:50:50 +0900

    직접 트랜잭션으로 db에 값 넣기

--- hello.jpa/src/main/java/hellojpa/Main.java
+++ hello.jpa/src/main/java/hellojpa/Main.java
@@ -1,12 +1,29 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import entity.Member;
 
 public class Main {
 	public static void main(String[] args) {
 	
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		
+		EntityManager em =  emf.createEntityManager();
+		
+		EntityTransaction tx = em.getTransaction(); //jpa에 모든 활동은 트랜잭션안에서 된다
+		tx.begin();
+		
+		Member member = new Member();
+		member.setId(100L);
+		member.setName("hello");
+		
+		em.persist(member);
+		
+		tx.commit();
 		
 		emf.close();
 		System.out.println("hello");
