47e8b00895e98a4e587dbad6a76e499867df05d7
UMK <haga5032@naver.com>
2023-05-01 11:52:15 +0900

    예외처리 구현

--- hello.jpa/src/main/java/hellojpa/Main.java
+++ hello.jpa/src/main/java/hellojpa/Main.java
@@ -9,24 +9,25 @@ import entity.Member;
 
 public class Main {
 	public static void main(String[] args) {
-	
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-		
-		EntityManager em =  emf.createEntityManager();
-		
-		EntityTransaction tx = em.getTransaction(); //jpa에 모든 활동은 트랜잭션안에서 된다
+
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction(); // jpa에 모든 활동은 트랜잭션안에서 된다
 		tx.begin();
-		
-		Member member = new Member();
-		member.setId(100L);
-		member.setName("hello");
-		
-		em.persist(member);
-		
-		tx.commit();
-		
-		emf.close();
+		try {
+			Member member = new Member();
+			member.setId(100L);
+			member.setName("hello");
+			em.persist(member);
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			emf.close();
+		}
 		System.out.println("hello");
-		
+
 	}
 }
